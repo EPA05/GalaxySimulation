992097943514811d5cccd25f275ffc672f925e9b
Elias Prest Andersen <eliaspandersen@outlook.dk>
2023-12-14 10:13:44 +0100

    Added the rest of the stars

--- src/Logic/LogicManager.java
+++ src/Logic/LogicManager.java
@@ -5,18 +5,53 @@ import processing.core.*;
 public class LogicManager {
   PApplet p;
   CenterMass centerMass;
-  Star star;
+  Star[] stars;
 
   public LogicManager(PApplet p) {
     this.p = p;
     centerMass = new CenterMass(p, 30, 2.19e8, p.color(255, 0, 0));
-    star = new Star(p, 0.45, p.color(255, 255, 0));
+    stars = new Star[16];
+
+    stars[0] = new Star(p, 0.45, p.color(255, 255, 0), centerMass);
+
+    stars[1] = new Star(p, 0.9, p.color(255, 255, 0), centerMass);
+
+    stars[2] = new Star(p, 1.35, p.color(255, 255, 0), centerMass);
+
+    stars[3] = new Star(p, 1.80, p.color(255, 255, 0), centerMass);
+
+    stars[4] = new Star(p, 2.25, p.color(255, 255, 0), centerMass);
+
+    stars[5] = new Star(p, 2.69, p.color(255, 255, 0), centerMass);
+
+    stars[6] = new Star(p, 3.14, p.color(255, 255, 0), centerMass);
+
+    stars[7] = new Star(p, 3.59, p.color(255, 255, 0), centerMass);
+
+    stars[8] = new Star(p, 4.04, p.color(255, 255, 0), centerMass);
+
+    stars[9] = new Star(p, 4.49, p.color(255, 255, 0), centerMass);
+
+    stars[10] = new Star(p, 4.94, p.color(255, 255, 0), centerMass);
+
+    stars[11] = new Star(p, 5.39, p.color(255, 255, 0), centerMass);
+
+    stars[12] = new Star(p, 5.84, p.color(255, 255, 0), centerMass);
+
+    stars[13] = new Star(p, 6.29, p.color(255, 255, 0), centerMass);
+
+    stars[14] = new Star(p, 6.74, p.color(255, 255, 0), centerMass);
+
+    stars[15] = new Star(p, 7.19, p.color(255, 255, 0), centerMass);
 
   }
 
   public void run() {
     centerMass.show();
-    star.show();
-  }
 
+    for (Star stars : stars) {
+      stars.show();
+      stars.update();
+    }
+  }
 }
--- src/Logic/Star.java
+++ src/Logic/Star.java
@@ -10,16 +10,21 @@ public class Star {
   PVector position;
   double pixelDistance;
   int colour;
+  double timeConstant;
+  PVector velocity;
   double kpcToMeters = 3.0857e19;
+  final double G = 6.674e-11;
 
-  public Star(PApplet p, Double d, int c) {
+  public Star(PApplet p, Double d, int c, CenterMass centerMass) {
     this.p = p;
+    this.centerMass = centerMass;
     distance = d * kpcToMeters;
     colour = c;
     radius = 15;
     pixelDistance = p.height / 2 / (7.3 * kpcToMeters);
     position = new PVector((float) (p.width / 2 + distance * pixelDistance), p.height / 2);
-
+    timeConstant = (60 * 60 * 24 * 365 * 1e6) / p.frameRate;
+    velocity = new PVector(0, 0);
   }
 
   public void show() {
@@ -27,10 +32,21 @@ public class Star {
     p.circle(position.x, position.y, radius);
   }
 
-  public void velocityCalculate() {
-    double G = 6.674e-11;
-    double velocity = Math.sqrt(G * centerMass.mass / distance);
-    System.out.println(velocity);
+  public void update() {
+    for (int i = 0; i < 100; i++) {
+      velocity(centerMass);
+      position.add(PVector.mult(velocity, (float) timeConstant));
+    }
+
   }
 
+  public void velocity(CenterMass centerMass) {
+    double orbitalSpeed = Math.sqrt((G * centerMass.mass) / (distance)); // Calculate the orbital speed of the planet
+    PVector directionToSun = PVector.sub(centerMass.position, position); // Vector from planet to sun
+    directionToSun.normalize(); // Normalize to get a unit vector
+    PVector VelocityDirection = directionToSun.rotate(PConstants.HALF_PI); // Rotate 90 degrees to get a
+    // perpendicular
+    velocity = VelocityDirection.mult((float) orbitalSpeed); // Calculate the velocity of the planet
+    velocity.mult((float) pixelDistance); // Convert velocity from m/s to pixels/s
+  }
 }
